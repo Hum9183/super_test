813bb34c504d192d457cac976525b009cb4628e6
Hum9183 <hum9183@gmail.com>
2024-06-22 12:04:35 +0900

    1

--- darkdragon.py
+++ darkdragon.py
@@ -4,5 +4,6 @@ from .dragon import Dragon
 
 class DarkDragon(Dragon):
     def __init__(self, hp, atk):
+        print(id(self.__class__), 'class of self')
         print(id(DarkDragon), 'in DarkDragon class')
         super(DarkDragon, self).__init__(hp, atk)
